f256dd3c3741591d99b6243b04013ea55f6c5600
SUNIL-KUMAWAT <kumawatsunil9494@gmail.com>
2022-12-30 13:00:35 +0530

    Add file Navbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -33,41 +33,17 @@ const Navbar = () => {
     //    {/* <Image objectFit={'cover'} position={'absolute'} maxH={'400px'} minH={'220px'} w={'100%'} zIndex={-1} className="item" data-value="1" src='/main002.png' alt="" role="presentation" />  */}
     // </Box>,
     // <Image w={'100%'} maxH={'600px'} objectFit='cover' minH={'180px'} zIndex={-1} className="item" data-value="2" src={{base:'./0001.png', md:'./001.png',lg:'./001.png'}} key="2" alt="" role="presentation" />,
-      <Image w={'100%'} mt={'10px'} maxH={'380px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./001.png' key="1" alt="" />,
-    <Image w={'100%'} mt={'10px'} maxH={'380px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./002.png' key="2" alt="" />,
-    <Image w={'100%'} mt={'10px'} maxH={'380px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./003.png' key="3" alt="" />,
-    <Image w={'100%'} mt={'10px'} maxH={'380px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./004.png' key="4" alt="" />,
-    <Image w={'100%'} mt={'10px'} maxH={'380px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./005.png' key="5" alt="" />,
+      <Image w={'100%'} mt={'10px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover',xl:'cover' }} zIndex={-1} src='./001.png' key="1" alt="" />,
+    <Image w={'100%'} mt={'10px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./002.png' key="2" alt="" />,
+    <Image w={'100%'} mt={'10px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./003.png' key="3" alt="" />,
+    <Image w={'100%'} mt={'10px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./004.png' key="4" alt="" />,
+    <Image w={'100%'} mt={'10px'} objectFit={{ base: 'contain', md: 'cover', lg: 'cover' }} zIndex={-1} src='./005.png' key="5" alt="" />,
 
   ];
 
   return (
-    <Box   style={{
-      backgroundImage: `url(bg8.jpg)`,
-      width: '100%',
-      // height: '100%',
-      backgroundSize: 'cover',
-      backgroundRepeat: 'no-repeat',
-      backgroundAttachment:'fixed'
-    }}>
-      <Box  bgColor={'white'} >
-        <AliceCarousel keyboardNavigation
-          // disableDotsControls
-          keysControlDisabled
-          // disableSlideInfo={false}
-          mouseDragEnabled={true}
-          autoPlayDirection="ltr"
-          disableButtonsControls
-          autoPlay
-          infinite
-          autoPlayInterval={3000}
-          animationDuration={1000}
-          mouseTracking items={items} />
-      </Box>
-      {/* <Box w={'100%'} h={'20px'}>
-        <Img  objectFit={'cover'} src='./dmlogo.jpg'></Img>
-      </Box> */}
-      <Box align='center' bgColor={'White'}
+    <Box>
+        <Box bgColor='blackAlpha.100' align='center' 
         borderTop={'5px solid #FFFF00'}
         borderBottom={'0px solid yellow '}
         // bgImage='url("./dmlogo.jpg")'
@@ -97,6 +73,24 @@ const Navbar = () => {
           <strong> We Provide Complete Digital Marketing Solutions</strong>
         </Text>
       </Box>
+      <Box  bgColor={'white'} >
+        <AliceCarousel keyboardNavigation
+          // disableDotsControls
+          keysControlDisabled
+          // disableSlideInfo={false}
+          mouseDragEnabled={true}
+          autoPlayDirection="ltr"
+          disableButtonsControls
+          autoPlay
+          infinite
+          autoPlayInterval={3000}
+          animationDuration={1000}
+          mouseTracking items={items} />
+      </Box>
+      {/* <Box w={'100%'} h={'20px'}>
+        <Img  objectFit={'cover'} src='./dmlogo.jpg'></Img>
+      </Box> */}
+    
     </Box>
   )
 }
